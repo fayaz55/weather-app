d0919408e12a57b48e42e27eba0f725c9cca6190
fayaz55 <fayazfahim1@gmail.com>
2021-02-21 20:13:06 -0500

    weather-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+const api ={
+  key:"7047d64e09cfd37300ef7d0afa092ac1",
+  base:"http://api.openweathermap.org/data/2.5/"
+}
 function App() {
+
+  const [query, setQuery] =useState('');
+  const [weather,setWeather]=useState('');
+  const search = evt =>{
+    if (evt.key==="Enter"){
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res=> res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
+  const dateBuilder = (d) => {
+  
+    let date = String(new window.Date());
+    date = date.slice(0,15)
+    return `${date}`
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= {(typeof weather.main != "undefined") 
+      ? ((weather.main.temp >15) ? 'App warm' : 'App') 
+      :'App' }>
+      <main>
+        <div className="search-box">
+          <input type="text"
+          className="search-bar"
+          placeholder="Search..."
+          onChange={e =>setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}/>
+        </div>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°C
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
+
+          <div className="info">
+              
+              <div className="column left">Feels Like</div>
+              <div className="column right">{Math.round(weather.main.feels_like)} °C</div>
+              
+              <div className="column left">High</div>
+              <div className="column right">{Math.round(weather.main.temp_max)} °C</div>
+              <div className="column left">Low</div>
+              <div className="column right">{Math.round(weather.main.temp_min)} °C</div>
+              <div className="column left">Humidity</div>
+              <div className="column right">{Math.round(weather.main.humidity)}%</div>
+              <div className="column left">Wind Speed</div>
+              <div className="column right">{Math.round(weather.wind.speed)} km/h</div>
+
+
+            </div>
+        </div>
+
+        ) : ('')}
+        </main>      
     </div>
   );
 }
